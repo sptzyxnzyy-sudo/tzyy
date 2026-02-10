e445bb4a9dac6e4e942121261669d86c8884df07
sptzyxnzyy-sudo <sptzyxnzyy@gmail.com>
2026-02-11 04:46:29 +0700

    Update sptzy.lua

--- sptzy.lua
+++ sptzy.lua
@@ -6,11 +6,11 @@ local lp = Players.LocalPlayer
 
 -- [[ SETTINGS ]] --
 local botActive = true
-local pullRadius = 150      -- Radius lebih jauh
+local pullRadius = 180      -- Radius lebih jauh
 local orbitHeight = 8      
 local orbitRadius = 10     
 local spinSpeed = 125        -- Putaran lebih kencang
-local followStrength = 100  -- Magnet sangat kencang (High Velocity)
+local followStrength = 200  -- Magnet sangat kencang (High Velocity)
 
 -- [[ LOGIKA PHYSICS & CONSTRAINT BREAKER ]] --
 local angle = 0
